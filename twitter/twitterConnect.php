b127ed03007a78d37d2a3196061101c229160d7b
Dorian Camilleri <doriancamilleri@yahoo.fr>
2013-11-09 22:01:30 +0100

    First Commit

---
+++ twitter/twitterConnect.php
@@ -0,0 +1,28 @@
+<?php
+require("lib/twitteroauth.php");  
+session_start(); 
+
+$consumerKey = 'lzljK0zCWnUlzqvERrtdiw';
+$consumerSecret = 'vZJMQXCzPZ42aa3F8ve6D7lqgoBm72froHDFJMuFpOQ';
+
+$callback = 'http://doriancamilleri.fr/countdown/index.php';
+
+$twitteroauth = new TwitterOAuth($consumerKey, $consumerSecret);  
+// Requesting authentication tokens, the parameter is the URL we will be redirected to  
+$request_token = $twitteroauth->getRequestToken($callback);  
+  
+// Saving them into the session  
+$_SESSION['oauth_token'] = $request_token['oauth_token'];  
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];  
+  
+// If everything goes well..  
+if($twitteroauth->http_code==200){  
+    // Let's generate the URL and redirect  
+    $url = $twitteroauth->getAuthorizeURL($request_token['oauth_token']); 
+    header('Location: '. $url); 
+} else { 
+    // It's a bad idea to kill the script, but we've got to know when there's an error.  
+    die('Something wrong happened.');  
+} 
+
+?>

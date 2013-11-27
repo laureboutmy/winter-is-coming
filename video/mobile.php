54ff201805598efd9ac9db0695b96b53037b8acb
Laure Boutmy <laureboutmy@gmail.com>
2013-11-28 00:36:44 +0100

    Mobile.php js
    Signed-off-by: Laure Boutmy <laureboutmy@gmail.com>

--- video/mobile.php
+++ video/mobile.php
@@ -69,13 +69,17 @@
 		</form>
 	</div>
 	<div id="tweet-feed">
-		<ul id="tweets"></ul>
+		<ul id="tweets">
+			
+		</ul>
 	</div>
 </section>
 <!-- SCRIPTS -->
 <script src="http://js.pusher.com/2.1/pusher.min.js"></script>
 <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="assets/scripts/libs/jquery.touchSwipe-1.6.5.min.js"></script>
+<script src="assets/scripts/libs/jquery.event.move.js"></script>
+<script src="assets/scripts/libs/jquery.event.swipe.js"></script>
+
 <script src="assets/scripts/libs/pusher.js"></script>
 <script src="assets/scripts/libs/tweet.js"></script>
 <script src="assets/scripts/libs/jquery.tweetMachine-0.2.1.js"></script>

dab408e1da8a0a3f6799e5025c83468a72d68816
Anthony <anthfr@users.noreply.github.com>
2013-11-28 22:22:37 +0100

    Callback + ini_set
    Signed-off-by: Anthony <anthfr@users.noreply.github.com>

--- video/assets/twitter/twitterConnect.php
+++ video/assets/twitter/twitterConnect.php
@@ -22,7 +22,7 @@
         $_SESSION['CONSUMER_KEY'] = 'lzljK0zCWnUlzqvERrtdiw';
         $_SESSION['CONSUMER_SECRET'] = 'vZJMQXCzPZ42aa3F8ve6D7lqgoBm72froHDFJMuFpOQ';
 
-        $callback = 'http://winter-is-coming.dev:8888/video/assets/twitter/twitterConnect.php';
+        $callback = 'http://laureboutmy.com/winter-is-coming/video/assets/twitter/twitterConnect.php';
         // $callback = 'http://doriancamilleri.fr/player/video/assets/twitter/twitterConnect.php';
 
         $twitteroauth = new TwitterOAuth($_SESSION['CONSUMER_KEY'], $_SESSION['CONSUMER_SECRET']);  
